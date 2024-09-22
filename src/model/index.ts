5f5f3e7a56803a40c73d48d15b69c26789d1153f
Adebayo Modupe Aisha <modupeadebayo001@gmail.com>
2024-09-22 22:17:27 +0100

    Delivers landing page for react class

---
+++ src/model/index.ts
@@ -0,0 +1,14 @@
+export interface IFAQ {
+   
+        id: string;
+        question: string;
+        answer: string;
+}
+
+export interface ITestimonial {
+    id: string;
+    comment: string;
+    avatarUrl: string;
+    name: string;
+    role: string;
+}
